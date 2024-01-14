b0ecee879546edef8516951d76ba1993792ba16a
Alexander Satchell <48191535+alexSatchell@users.noreply.github.com>
2024-01-14 15:26:03 -0500

    Wrapping up a section on pointers
    that really didn't talk ab pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,15 +1,50 @@
 package pointers
 
+import (
+	"errors"
+	"fmt"
+)
+
+// Go allows for creating of new types (im guessing alias's)
+// from existing ones
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// In Go, when you call a function (like most langs ik of)
+// the argunmemtns are copied
+
 type Wallet struct {
 	// if a symbol is lowercased like balance is,
 	// it will be private, only accessible by methods
-	balance int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// fmt.Printf("address if balance in deposit is %p \n", &w.balance)
+	// In go, we do not need to explicitly de-referance
+	// furthermore, struct pointers are auto de-referenced
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+// var keyword will be a global variable to the package
+var ErrorInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrorInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,15 +1,62 @@
 package pointers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
 
+		want := Bitcoin(10)
+
+		assetBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		want := Bitcoin(10)
+
+		assetBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrorInsufficientFunds)
+		assetBalance(t, wallet, Bitcoin(20))
+	})
+}
+
+func assetBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	fmt.Printf("wallet mem address: %p \n", &wallet)
+
+	t.Helper()
 	got := wallet.Balance()
-	want := 10
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("didn't get an error, but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+/*
+* Accoring to lesson plan, nil is synonymous with Null, I have however heard otherwise
+* Like null, if you try to access a value that is nil, it ill throw a runtime panic.
+ */
