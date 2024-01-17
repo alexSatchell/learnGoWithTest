5248ae66f841cc7d784471bc8384bd1f7ff44dd0
Alexander Satchell <48191535+alexSatchell@users.noreply.github.com>
2024-01-17 07:19:01 -0500

    Continued work with maps

--- maps/search.go
+++ maps/search.go
@@ -7,8 +7,8 @@ var ErrNotFound = errors.New("could not find the word you were looking for")
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	// maps can return two values, the second value, the second
-	// value is a bool, which indicates a successful find
+	// maps can return two values, the second value is a bool,
+	// which indicates a successful find
 	definition, ok := d[word]
 
 	if !ok {
@@ -16,4 +16,14 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+/*
+* Notice that there is not pointer to Dictionary,
+* In GO, maps are LIKE reference types, BUT THEY ARE NOT
+* when you pass a map to a function, you are indeed passing
+* a copy of the map, but this copy includes the pointer to the underlying data structure.
+ */
+func (d Dictionary) Add(word string, definition string) {
+	d[word] = definition
 }
--- maps/search_test.go
+++ maps/search_test.go
@@ -5,24 +5,52 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	// t.Run("Search known word", func(t *testing.T) {
-	// 	_, got := dictionary.Search("test")
-	// 	want := "this is just a test"
-	//
-	// 	assertStrings(t, got, want)
-	// })
+	t.Run("Search known word", func(t *testing.T) {
+		definition, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, definition, want)
+	})
 
 	t.Run("Search unknown word", func(t *testing.T) {
 		_, got := dictionary.Search("unknown")
 
-		assertStrings(t, got, ErrNotFound)
+		asserError(t, got, ErrNotFound)
 	})
 }
 
-func assertStrings(t testing.TB, got, want error) {
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+	asserDefinition(t, dictionary, word, definition)
+}
+
+// helpers
+func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func asserError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func asserDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("Should find added word")
+	}
+	assertStrings(t, got, definition)
 }
